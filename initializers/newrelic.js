cbba020ca803d230765fa1acda83b226fe1046e9
David Larrimore <david.larrimore@gsa.gov>
2015-03-27 22:30:53 -0400

    updated to use new relic plugin

--- initializers/newrelic.js
+++ initializers/newrelic.js
@@ -1,34 +1,31 @@
-newrelic = require("newrelic");
+var newrelic = require("newrelic");
 
-exports.newrelic = function(api, next){
+module.exports = {
+  initialize: function(api, next){
+    api.newrelic = {
+      middleware: function(connection, actionTemplate, next){
+        if(connection.type === 'web'){
+          // for now, the node newrelic agent only supports HTTP requests
+          newrelic.setTransactionName(actionTemplate.name);
+        }
+        next(connection, true);
+      },
 
-  api.newrelic = {};
+      errorReporter: function(type, err, extraMessages, severity){
+        newrelic.noticeError(err);
+      }
+    };
 
-  api.newrelic.middleware = function(connection, actionTemplate, next){
-    if(connection.type === 'web'){
-      // for now, the node newrelic agent only supports HTTP requests
-      newrelic.setTransactionName(actionTemplate.name);
-    }
-    next(connection, true);
-  }
+    next();
+  },
 
-  api.newrelic.errorReporter = function(type, err, extraMessages, severity){
-    newrelic.noticeError(err);
-  }
-
-  api.newrelic._start = function(api, next){
+  start: function(api, next){
     // load the newrelic middleware into actionhero
-    api.actions.preProcessors.push( api.newrelic.middleware );
+    api.actions.addPreProcessor( api.newrelic.middleware );
     // load the newrelic error reporter into actionhero
     api.exceptionHandlers.reporters.push( api.newrelic.errorReporter );
     // optional: ignore certain actions
     // newrelic.setIgnoreTransaction('actionName');
     next();
-  };
-
-  api.newrelic._stop =  function(api, next){
-    next();
-  };
-
-  next();
-}
+  }
+};
