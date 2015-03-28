4f52e6ef94b18fe725e8093f7ad94d17e01b02ca
David Larrimore <david.larrimore@gsa.gov>
2015-03-27 22:03:47 -0400

    New Relic Integration

--- actions/getForexDataFromSequelize.js
+++ actions/getForexDataFromSequelize.js
@@ -6,8 +6,107 @@ exports.getForexdataFromSequelize = {
   name: 'getForexdataFromSequelize',
   description: 'return latest Forex data from Yahoo',
 
-  outputExample:{"query":{"count":2,"created":"2015-03-12T02:16:27Z","lang":"en-US","diagnostics":{"url":[{"execution-start-time":"1","execution-stop-time":"2","execution-time":"1","content":"http://www.datatables.org/yahoo/finance/yahoo.finance.xchange.xml"},{"execution-start-time":"3006","execution-stop-time":"3013","execution-time":"7","content":"http://download.finance.yahoo.com/d/quotes.csv?s=EURUSD=X&f=snl1d1t1ab"},{"execution-start-time":"3006","execution-stop-time":"3013","execution-time":"7","content":"http://download.finance.yahoo.com/d/quotes.csv?s=GBPUSD=X&f=snl1d1t1ab"}],"publiclyCallable":"true","cache":[{"error":"Timed out waiting for operation - failing node: localhost/127.0.0.1:11211","execution-start-time":"505","execution-stop-time":"3006","execution-time":"2501","method":"GET","type":"MEMCACHED","content":"8251a0ff2fae33ba24cbed4509e06a2a"},{"error":"Timed out waiting for operation - failing node: localhost/127.0.0.1:11211","execution-start-time":"506","execution-stop-time":"3006","execution-time":"2500","method":"GET","type":"MEMCACHED","content":"5372a187be7ec34f4e273188d21b0c77"}],"javascript":["Unable to retrieve query results from cache, Timed out waiting for operation - failing node: localhost/127.0.0.1:11211","Unable to retrieve query results from cache, Timed out waiting for operation - failing node: localhost/127.0.0.1:11211",{"execution-start-time":"504","execution-stop-time":"3013","execution-time":"2509","instructions-used":"35143","table-name":"yahoo.finance.xchange"},{"execution-start-time":"504","execution-stop-time":"3014","execution-time":"2509","instructions-used":"37324","table-name":"yahoo.finance.xchange"}],"query":[{"execution-start-time":"3006","execution-stop-time":"3013","execution-time":"7","content":"select * from csv where url='http://download.finance.yahoo.com/d/quotes.csv?s=EURUSD=X&f=snl1d1t1ab' and columns='Symbol,Name,Rate,Date,Time,Ask,Bid'"},{"execution-start-time":"3006","execution-stop-time":"3014","execution-time":"8","content":"select * from csv where url='http://download.finance.yahoo.com/d/quotes.csv?s=GBPUSD=X&f=snl1d1t1ab' and columns='Symbol,Name,Rate,Date,Time,Ask,Bid'"}],"user-time":"3015","service-time":"5016","build-version":"0.2.58"},"results":{"rate":[{"id":"EURUSD","Name":"EUR/USD","Rate":"1.0515","Date":"3/12/2015","Time":"2:16am","Ask":"1.0515","Bid":"1.0515"},{"id":"GBPUSD","Name":"GBP/USD","Rate":"1.4920","Date":"3/12/2015","Time":"2:16am","Ask":"1.4922","Bid":"1.4917"}]}}},
-
+  outputExample:{
+  "results": [
+    {
+      "id": 1,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "EURUSD",
+      "name": "EUR/USD",
+      "date": "3/25/2015",
+      "time": "0:56am",
+      "rate": 1.0974,
+      "bid": 1.0974,
+      "ask": 1.0675
+    },
+    {
+      "id": 2,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "GBPUSD",
+      "name": "GBP/USD",
+      "date": "3/24/2015",
+      "time": "0:57am",
+      "rate": 1.0974,
+      "bid": 1.0874,
+      "ask": 1.0775
+    },
+    {
+      "id": 3,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "EURUSD",
+      "name": "EUR/USD",
+      "date": "3/23/2015",
+      "time": "0:58am",
+      "rate": 1.0974,
+      "bid": 1.0774,
+      "ask": 1.0875
+    },
+    {
+      "id": 4,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "GBPUSD",
+      "name": "GBP/USD",
+      "date": "3/22/2015",
+      "time": "0:59am",
+      "rate": 1.0974,
+      "bid": 1.0674,
+      "ask": 1.0975
+    },
+    {
+      "id": 5,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "EURUSD",
+      "name": "EUR/USD",
+      "date": "3/25/2015",
+      "time": "0:56am",
+      "rate": 1.0974,
+      "bid": 1.0974,
+      "ask": 1.0675
+    },
+    {
+      "id": 6,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "GBPUSD",
+      "name": "GBP/USD",
+      "date": "3/24/2015",
+      "time": "0:57am",
+      "rate": 1.0974,
+      "bid": 1.0874,
+      "ask": 1.0775
+    },
+    {
+      "id": 7,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "EURUSD",
+      "name": "EUR/USD",
+      "date": "3/23/2015",
+      "time": "0:58am",
+      "rate": 1.0974,
+      "bid": 1.0774,
+      "ask": 1.0875
+    },
+    {
+      "id": 8,
+      "createdAt": "0000-00-00 00:00:00",
+      "updatedAt": "0000-00-00 00:00:00",
+      "code": "GBPUSD",
+      "name": "GBP/USD",
+      "date": "3/22/2015",
+      "time": "0:59am",
+      "rate": 1.0974,
+      "bid": 1.0674,
+      "ask": 1.0975
+    }
+  ]},
+    
+    
   run: function(api, connection, next){    
      
      //var db = api.sequelize;  
